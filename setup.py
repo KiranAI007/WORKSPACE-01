eec3836693fb2be36941c5a3bc2d8237a745090d
KiranAI007 <kiranrkrishnan07@gmail.com>
2024-02-03 11:18:21 +0530

    version name updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 
-__version__ = "0.0.1"
+__version__ = "0.0.0"
 REPO_NAME = "WORKSPACE-01"
 PKG_NAME = "db_connect"
 AUTHOR_USER_NAME = "KiranAI007"
